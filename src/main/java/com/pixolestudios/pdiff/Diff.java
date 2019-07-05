8a110456026c77203ba4f96a5dcf41f42e5052d5
jrickard <joerickard@hotmail.co.uk>
2019-07-05 09:03:54 +0100

    Updated diff image generation alpha

--- src/main/java/com/pixolestudios/pdiff/Diff.java
+++ src/main/java/com/pixolestudios/pdiff/Diff.java
@@ -24,15 +24,15 @@ public class Diff {
     }
 
     protected void CalculateDiff(boolean ignoreColor, boolean generateDiffImg) {
-        if(image1.getWidth() != image2.getWidth() || image1.getHeight() != image2.getHeight()){
+        if (image1.getWidth() != image2.getWidth() || image1.getHeight() != image2.getHeight()) {
             System.out.print("different size images");
         } else {
             CompareImgs(ignoreColor, generateDiffImg);
         }
     }
 
-    private void CompareImgs(boolean ignoreColor, boolean generateDiffImg){
-        if (generateDiffImg){
+    private void CompareImgs(boolean ignoreColor, boolean generateDiffImg) {
+        if (generateDiffImg) {
             diffImg = new BufferedImage(image1.getWidth(), image1.getHeight(), BufferedImage.TYPE_INT_ARGB);
         }
         boolean foundDiffAtPixel = false;
@@ -40,36 +40,64 @@ public class Diff {
         for (int i = 0; i < image1.getWidth(); i++) {
             for (int j = 0; j < image1.getHeight(); j++) {
                 foundDiffAtPixel = false;
-                if (image1.getRGB(i, j) != image2.getRGB(i, j)){
+                if (image1.getRGB(i, j) != image2.getRGB(i, j)) {
 //                    System.out.print("diff found at " + i + ", " + j);
                     foundDiffAtPixel = true;
-                    numDiffs ++;
+                    numDiffs++;
                 }
-                if (generateDiffImg){
+                if (generateDiffImg) {
                     GenerateDiffImg(i, j, foundDiffAtPixel);
                 }
             }
         }
-        if (generateDiffImg){
+        if (generateDiffImg) {
             try {
                 ImageIO.write(diffImg, "png", new File("output.png"));
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-        System.out.print(CalcPercentageDiff(numDiffs, image1.getHeight() * image1.getWidth()) + "% diff");
+        System.out.println(CalcPercentageDiff(numDiffs, image1.getHeight() * image1.getWidth()) + "% diff");
+        System.out.print(CalcPercentageMatch(numDiffs, image1.getHeight() * image1.getWidth()) + "% match");
     }
 
     private void GenerateDiffImg(int x, int y, boolean isDiffPixel) {
-        if (!isDiffPixel){
-            diffImg.setRGB(x, y, image1.getRGB(x, y));
+        if (!isDiffPixel) {
+            diffImg.setRGB(x, y, toRGB(getPixelAlpha(image1.getRGB(x, y)) / 2,
+                    getPixelRed(image1.getRGB(x, y)),
+                    getPixelGreen(image1.getRGB(x, y)),
+                    getPixelBlue(image1.getRGB(x, y))));
         } else {
             diffImg.setRGB(x, y, diffColor);
         }
     }
 
-    private float CalcPercentageDiff(int numDiffPixels, int totalNoPixels){
-        return (((float) numDiffPixels) /((float) totalNoPixels)) * 100;
+    private float CalcPercentageDiff(int numDiffPixels, int totalNoPixels) {
+        return (((float) numDiffPixels) / ((float) totalNoPixels)) * 100;
+    }
+
+    private float CalcPercentageMatch(int numDiffPixels, int totalNoPixels) {
+        return (((float) totalNoPixels - numDiffPixels) / ((float) totalNoPixels)) * 100;
+    }
+
+    private int getPixelAlpha(int pixel) {
+        return (pixel >> 24) & 0xff;
+    }
+
+    private int getPixelRed(int pixel) {
+        return (pixel >> 16) & 0xff;
+    }
+
+    private int getPixelGreen(int pixel) {
+        return (pixel >> 8) & 0xff;
+    }
+
+    private int getPixelBlue(int pixel) {
+        return (pixel) & 0xff;
+    }
+
+    private int toRGB(int a, int r, int g, int b) {
+        return (a << 24) | (r << 16) | (g << 8) | b;
     }
 
 }
